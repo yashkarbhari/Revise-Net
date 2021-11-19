5f3ef206f0956cab4b8fd77ab2acf5246f10d5a1
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2021-11-19 17:38:04 +0530

    Create training.py

---
+++ train/training.py
@@ -0,0 +1,56 @@
+''' 
+This file is to save training optimization, which further can be used to
+start the training if the runtime disconnects.  
+'''
+
+
+print("---start training...")
+ite_num = 0
+running_loss = 0.0
+running_tar_loss = 0.0
+ite_num4val = 0
+
+for epoch in range(0, epoch_num):
+    net.train()
+
+    for i, data in enumerate(salobj_dataloader):
+        ite_num = ite_num + 1
+        ite_num4val = ite_num4val + 1
+
+        inputs, labels = data['image'], data['label']
+
+        inputs = inputs.type(torch.FloatTensor)
+        labels = labels.type(torch.FloatTensor)
+
+        # wrap them in Variable
+        if torch.cuda.is_available():
+            inputs_v, labels_v = Variable(inputs.cuda(), requires_grad=False), Variable(labels.cuda(),
+                                                                                        requires_grad=False)
+        else:
+            inputs_v, labels_v = Variable(inputs, requires_grad=False), Variable(labels, requires_grad=False)
+
+        optimizer.zero_grad()
+
+        d0, d1, d2, d3, d4, d5, d6, d7 = net(inputs_v)
+        loss2, loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, d7, labels_v)
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        running_tar_loss += loss2.item()
+
+        del d0, d1, d2, d3, d4, d5, d6, d7, loss2, loss
+
+        print("[epoch: %3d/%3d, batch: %5d/%5d, ite: %d] train loss: %3f, tar: %3f " % (
+        epoch + 1, epoch_num, (i + 1) * batch_size_train, train_num, ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
+
+        if ite_num % 1000 == 0:  # save model every 2000 iterations
+            
+            state = {'epoch': epoch + 1, 'state_dict': net.state_dict(),
+            'optimizer': optimizer.state_dict()}
+            torch.save(state, model_dir + "revisenet_ecssd_itr_%d_train_%3f_tar_%3f.pth" % (ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
+            running_loss = 0.0
+            running_tar_loss = 0.0
+            net.train() 
+            ite_num4val = 0
