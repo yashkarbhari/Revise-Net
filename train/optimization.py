a0e49df7cc3fe20f4d969dad4effb7b7a524c45e
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2021-11-19 17:32:14 +0530

    Create optimization.py

---
+++ train/optimization.py
@@ -0,0 +1,46 @@
+bce_loss = nn.BCELoss(size_average=True)
+ssim_loss = SSIM(window_size=11,size_average=True)
+iou_loss = IOU(size_average=True)
+
+def bce_ssim_loss(pred,target):
+
+	bce_out = bce_loss(pred,target)
+	ssim_out = 1 - ssim_loss(pred,target)
+	iou_out = iou_loss(pred,target)
+
+	loss = bce_out + ssim_out + iou_out
+
+	return loss
+
+def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, d7, labels_v):
+
+	loss0 = bce_ssim_loss(d0,labels_v)
+	loss1 = bce_ssim_loss(d1,labels_v)
+	loss2 = bce_ssim_loss(d2,labels_v)
+	loss3 = bce_ssim_loss(d3,labels_v)
+	loss4 = bce_ssim_loss(d4,labels_v)
+	loss5 = bce_ssim_loss(d5,labels_v)
+	loss6 = bce_ssim_loss(d6,labels_v)
+	loss7 = bce_ssim_loss(d7,labels_v)
+	#ssim0 = 1 - ssim_loss(d0,labels_v)
+
+	# iou0 = iou_loss(d0,labels_v)
+	#loss = torch.pow(torch.mean(torch.abs(labels_v-d0)),2)*(5.0*loss0 + loss1 + loss2 + loss3 + loss4 + loss5) #+ 5.0*lossa
+	loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6 + loss7#+ 5.0*lossa
+	print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"%(loss0.item(),loss1.item(),loss2.item(),loss3.item(),loss4.item(),loss5.item(),loss6.item()))
+	# print("BCE: l1:%3f, l2:%3f, l3:%3f, l4:%3f, l5:%3f, la:%3f, all:%3f\n"%(loss1.data[0],loss2.data[0],loss3.data[0],loss4.data[0],loss5.data[0],lossa.data[0],loss.data[0]))
+
+	return loss0, loss
+
+net = ReviseNet(3, 1)
+if torch.cuda.is_available():
+    net.cuda()
+
+   
+print("---define optimizer...")
+optimizer = optim.Adam(net.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+
+
+
+
+
