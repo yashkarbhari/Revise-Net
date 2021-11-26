6b52f7296c0417ce8321fb13eed74041b19fbd4c
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2021-11-26 21:39:29 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import os
+from skimage import io, transform
+import torch
+import torchvision
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms#, utils
+# import torch.optim as optim
+
+import numpy as np
+from PIL import Image
+import glob
